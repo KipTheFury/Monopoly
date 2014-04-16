038e8ed6af9f1eefd6558c4a1a803ee68018b9b8
kbennett <kyle.bennett@piksel.com>
2014-04-16 12:11:02 +0100

    Initial creation of project

---
+++ monopoly/src/main/java/com/kb/monopoly/game/Game.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.kb.monopoly.game;
+
+/**
+ * @author kbennett
+ * 
+ */
+public class Game {
+
+    public Game() {
+
+    }
+}
---
+++ monopoly/src/test/java/com/kb/monopoly/game/GameTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.kb.monopoly.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kbennett
+ * 
+ */
+public class GameTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public void canCreateGame() {
+        Game g = new Game();
+    }
+
+}
